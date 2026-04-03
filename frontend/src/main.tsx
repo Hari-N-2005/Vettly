ed0216dc2414acbae91e84b369f82089104017db
= <harigovindn700@gmail.com>
2026-04-03 13:29:13 +0530

    added minor v tweaks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
 import { TenderProvider } from '@/context/TenderContext'
 import './styles/global.css'
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TenderProvider>
       <App />
+      <Analytics />
     </TenderProvider>
   </React.StrictMode>,
 )
